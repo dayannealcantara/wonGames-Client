8bfde9610dd43268a0132ab3a279d239ee6ff86a
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-24 15:26:30 -0300

    feat: testing checkbox component

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,33 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+
+import theme from 'styles/theme'
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render with label', () => {
+    renderWithTheme(<Checkbox label="checkbox label" labelFor="check" />)
+
+    expect(screen.getByRole('checkbox')).toBeInTheDocument()
+
+    expect(screen.getByLabelText(/checkbox label/i)).toBeInTheDocument()
+
+    expect(screen.getByText(/checkbox label/i)).toHaveAttribute('for', 'check')
+  })
+
+  it('should render without label', () => {
+    renderWithTheme(<Checkbox />)
+
+    expect(screen.queryByLabelText('Checkbox')).not.toBeInTheDocument()
+  })
+
+  it('should render with black label', () => {
+    renderWithTheme(
+      <Checkbox label="checkbox label" labelFor="check" labelColor="black" />
+    )
+
+    expect(screen.getByText(/checkbox label/i)).toHaveStyle({
+      color: theme.colors.black
+    })
+  })
+})
