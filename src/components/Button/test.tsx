29f2c3ed6c79febd247a7c9b0eb4453a1447b993
dayannealcantara <dayannealcantara14@gmail.com>
2023-05-24 14:31:56 -0300

    feat: testing button for text button variation

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,8 +1,8 @@
 import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
 import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 import Button from '.'
-import { renderWithTheme } from 'utils/tests/helper'
 
 describe('<Button />', () => {
   it('should render the medium size by default', () => {
@@ -51,6 +51,27 @@ describe('<Button />', () => {
 
     expect(screen.getByText(/buy now/i)).toBeInTheDocument()
     expect(screen.getByTestId('icon')).toBeInTheDocument()
+  })
+
+  it('should render a minimal version', () => {
+    renderWithTheme(
+      <Button icon={<AddShoppingCart data-testid="icon" />} minimal>
+        Buy now
+      </Button>
+    )
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      background: 'none',
+      color: '#F231A5'
+    })
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyleRule(
+      'background',
+      'none',
+      {
+        modifier: ':hover'
+      }
+    )
   })
 
   it('should render Button as a link', () => {
