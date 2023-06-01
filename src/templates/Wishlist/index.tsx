6f1681e0523b8bbaae865553ef9378ab955dfe27
dayannealcantara <dayannealcantara14@gmail.com>
2023-06-01 12:00:28 -0300

    feat: adding and testing games on the wishlist page

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -3,15 +3,18 @@ import Base from 'templates/Base'
 import Heading from 'components/Heading'
 import Showcase from 'components/Showcase'
 import { Container } from 'components/Container'
-import { GameCardProps } from 'components/GameCard'
+import GameCard, { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
+import { Grid } from 'components/Grid'
 
 export type WishlistTemplateProps = {
+  games?: GameCardProps[]
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
 }
 
 const Wishlist = ({
+  games,
   recommendedGames,
   recommendedHighlight
 }: WishlistTemplateProps) => (
@@ -20,6 +23,11 @@ const Wishlist = ({
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
+      <Grid>
+        {games?.map((game, index) => (
+          <GameCard key={index} {...game} />
+        ))}
+      </Grid>
     </Container>
 
     <Showcase
