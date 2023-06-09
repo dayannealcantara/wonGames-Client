97d4cb3d3a65cdbb8c48aa82319604477b1a3f82
dayannealcantara <dayannealcantara14@gmail.com>
2023-06-09 15:13:28 -0300

    feat: adjust us textField test

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -8,7 +8,7 @@ import TextField from '.'
 
 describe('<TextField />', () => {
   it('Renders with Label', () => {
-    renderWithTheme(<TextField label="Label" labelFor="Field" id="Field" />)
+    renderWithTheme(<TextField label="Label" name="Label" />)
 
     expect(screen.getByLabelText('Label')).toBeInTheDocument()
   })
@@ -42,12 +42,7 @@ describe('<TextField />', () => {
   it('Changes its value when typing', async () => {
     const onInput = jest.fn()
     renderWithTheme(
-      <TextField
-        onInput={onInput}
-        label="TextField"
-        labelFor="TextField"
-        id="TextField"
-      />
+      <TextField onInput={onInput} label="TextField" name="TextField" />
     )
 
     const input = screen.getByRole('textbox')
@@ -67,8 +62,7 @@ describe('<TextField />', () => {
       <TextField
         onInput={onInput}
         label="TextField"
-        labelFor="TextField"
-        id="TextField"
+        name="TextField"
         disabled
       />
     )
@@ -90,7 +84,6 @@ describe('<TextField />', () => {
       <TextField
         icon={<Email data-testid="icon" />}
         label="TextField"
-        labelFor="TextField"
         error="Error message"
       />
     )
@@ -98,17 +91,5 @@ describe('<TextField />', () => {
     expect(screen.getByText('Error message')).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
-  })
-
-  it('Is accessible by tab', () => {
-    renderWithTheme(
-      <TextField label="TextField" labelFor="TextField" id="TextField" />
-    )
-
-    const input = screen.getByLabelText('TextField')
-    expect(document.body).toHaveFocus()
-
-    userEvent.tab()
-    expect(input).not.toHaveFocus()
   })
 })
