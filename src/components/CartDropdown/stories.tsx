cd74250bd8db3440c51a201499a54bb20ed6d252
dayannealcantara <dayannealcantara14@gmail.com>
2023-08-03 21:10:12 -0300

    feat:creating option to show empty cart

--- src/components/CartDropdown/stories.tsx
+++ src/components/CartDropdown/stories.tsx
@@ -1,23 +1,30 @@
-import { Story, Meta } from '@storybook/react'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import CartDropdown, { CartDropdownProps } from '.'
+
 import items from 'components/CartList/mock'
 
 export default {
   title: 'CartDropdown',
   component: CartDropdown,
   args: {
-    items: items,
+    items,
     total: 'R$ 300,00'
   },
   parameters: {
-    backgrounds: { default: 'won-dark' }
+    backgrounds: {
+      default: 'won-dark'
+    }
   }
 } as Meta
 
 export const Default: Story<CartDropdownProps> = (args) => (
-  <div
-    style={{ maxWidth: ' 98%', display: 'flex', justifyContent: 'flex-end' }}
-  >
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
     <CartDropdown {...args} />
+  </div>
+)
+
+export const Empty: Story<CartDropdownProps> = () => (
+  <div style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}>
+    <CartDropdown />
   </div>
 )
