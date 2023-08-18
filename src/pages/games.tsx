9a22dd561d1ab0505c72f7fa0f14bd45222bed4f
dayannealcantara <dayannealcantara14@gmail.com>
2023-08-17 21:22:26 -0300

    feature:creating game query

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,16 +1,34 @@
-import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
-import gamesMock from 'components/GameCardSlider/mock'
+import GamesTemplate, { GamesTemplateProps } from "templates/Games";
+import filterItemsMock from "components/ExploreSidebar/mock";
+import { initializeApollo } from "utils/apollo";
+import { QUERY_GAMES } from "graphql/queries/gamesQuery";
 
 export default function GamesPage(props: GamesTemplateProps) {
-  return <GamesTemplate {...props} />
+  return <GamesTemplate {...props} />;
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo();
+
+  const { data } = await apolloClient.query({
+    query: QUERY_GAMES,
+    variables: { limit: 9 },
+  });
+
   return {
     props: {
-      games: gamesMock,
-      filterItems: filterItemsMock
-    }
-  }
+      revalidate: 60,
+      games: data.games.map((game) => ({
+        title: game.name,
+        slug: game.slug,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover.url}`,
+        price: new Intl.NumberFormat("en", {
+          style: "currency",
+          currency: "USD",
+        }).format(game.price),
+      })),
+      filterItems: filterItemsMock,
+    },
+  };
 }
